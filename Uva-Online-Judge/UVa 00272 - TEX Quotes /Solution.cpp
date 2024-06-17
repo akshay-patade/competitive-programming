1c8f5ee84679f756734260c4715e6b9dc6b04c51
Akshay Patade <akshaypatade@gmail.com>
2024-06-17 12:45:33 -0400

    Tex Quotes solution

---
+++ Uva-Online-Judge/UVa 00272 - TEX Quotes /Solution.cpp
@@ -0,0 +1,48 @@
+
+#define _USE_MATH_DEFINES
+#include<iostream>
+#include<climits>
+#include<algorithm>
+#include<set>
+#include<unordered_map>
+#include<queue>
+#include<cmath>
+#include<map>
+#include<array>
+#include<regex>
+#include<string>
+
+using namespace std;
+
+
+int main() {
+
+	#ifndef ONLINE_JUDGE
+  		freopen("ip1.txt", "r", stdin);
+  		freopen("out1.txt", "w", stdout);
+	#endif
+
+   string s;
+   
+   while( getline(cin, s)) {
+
+    string result = "";
+    int pos = s.find('"');
+    bool open = true;
+    
+    while(pos != -1) {
+    
+        if(pos != 0)
+            result = result + s.substr(0, pos);
+
+        result += open ? "``" : "''";
+        open = !open;
+        s = s.substr(pos + 1, s.length() - pos);
+        pos = s.find('"');
+    }
+
+    result += s;
+    cout<<result<<endl;
+    }
+
+}
